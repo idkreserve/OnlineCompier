40fe02c940ad7e9dec208ff8a7ae2a4f485827d8
zerostar <sanina228user@gmail.com>
2021-06-16 03:01:10 +0300

    add new routes

--- app/views.py
+++ app/views.py
@@ -1,8 +1,22 @@
-import re
 from app import app
-from flask import render_template
+from flask import render_template, session, url_for
+from os import path, stat
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.html')
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = path.join(app.root_path, endpoint, filename)
+            values['q'] = int(stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
 
